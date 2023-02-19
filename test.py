1f5814d7f2a80a069d18131095c52c8b8f9def5d
Emilio Ziniades <emilioziniades@protonmail.com>
2023-02-19 16:24:02 +0200

    improve tests

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ import unittest
 class TestQuine(unittest.TestCase):
     def test_quine_once(self):
         cmd = ["python3", "quine.py"]
-        quine = pathlib.Path("quine.py").read_bytes()
         quined = subprocess.run(cmd, stdout=subprocess.PIPE).stdout
+        quine = pathlib.Path("quine.py").read_bytes()
         self.assertEqual(quine, quined)
 
     def test_quine_many_times(self):
         cmd = ["python3", "quine.py"] + ["|", "python3"] * 100
-        quine = pathlib.Path("quine.py").read_bytes()
         quined = subprocess.run(cmd, stdout=subprocess.PIPE).stdout
+        quine = pathlib.Path("quine.py").read_bytes()
         self.assertEqual(quine, quined)
 
 
