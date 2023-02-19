f92ad9a1461faf004ca457db988c6a05542c4457
Emilio Ziniades <emilioziniades@protonmail.com>
2023-02-19 16:23:06 +0200

    better test names

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ import unittest
 class TestQuine(unittest.TestCase):
     def test_quine_once(self):
         cmd = ["python3", "quine.py"]
-        quined = subprocess.run(cmd, stdout=subprocess.PIPE).stdout
         quine = pathlib.Path("quine.py").read_bytes()
+        quined = subprocess.run(cmd, stdout=subprocess.PIPE).stdout
         self.assertEqual(quine, quined)
 
     def test_quine_many_times(self):
         cmd = ["python3", "quine.py"] + ["|", "python3"] * 100
-        quined = subprocess.run(cmd, stdout=subprocess.PIPE).stdout
         quine = pathlib.Path("quine.py").read_bytes()
+        quined = subprocess.run(cmd, stdout=subprocess.PIPE).stdout
         self.assertEqual(quine, quined)
 
 
